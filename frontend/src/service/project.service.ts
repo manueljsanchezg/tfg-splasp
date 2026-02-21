b74138e56eb025a180adc9481a5a8e7c3b413bb4
manueljsanchezg <manueljsanchez10@gmail.com>
2026-02-21 17:50:57 +0100

    feat: Added the connection with the backend

---
+++ frontend/src/service/project.service.ts
@@ -0,0 +1,19 @@
+import { api } from "./api";
+
+export const analyzeProject = async (project: File) => {
+    try {
+        const formData = new FormData()
+        formData.append("file", project)
+        const response = await api.post('/projects/analyze',
+            formData,
+            {
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
+            }
+        )
+        return response.data
+    } catch (error) {
+        console.error("Error analyzing project", error)
+    }
+}
