428497d414a5ba224b6958c5910fec20c16f713a
manueljsanchezg <manueljsanchez10@gmail.com>
2026-02-21 17:47:34 +0100

    feat: add cors middlware to connect frontend and backend

--- backend/app/app.py
+++ backend/app/app.py
@@ -3,8 +3,22 @@ from fastapi import FastAPI
 from app.projects.routes import router as project_routes
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
 
-app.include_router(project_routes)
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(project_routes, prefix="/api")
 
 @app.get("/health")
 async def root():
