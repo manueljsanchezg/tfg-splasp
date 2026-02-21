dde1604eb6da921e2ab1af28381a5aee0b1edad1
manueljsanchezg <manueljsanchez10@gmail.com>
2026-02-21 19:40:57 +0100

    feat: update app with lifespan to create the database

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,9 +1,25 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.db import Base, engine
+from app.user.routes import router as user_routes
 from app.projects.routes import router as project_routes
 
-app = FastAPI()
-from fastapi.middleware.cors import CORSMiddleware
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        print("Creating table")
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    await engine.dispose()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
 
 origins = [
     "http://localhost:5173",
@@ -18,7 +34,8 @@ app.add_middleware(
 )
 
 
-app.include_router(project_routes, prefix="/api")
+app.include_router(user_routes)
+app.include_router(project_routes)
 
 @app.get("/health")
 async def root():
--- backend/app/projects/routes.py
+++ backend/app/projects/routes.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, UploadFile
 
 from app.core.splasp import analyze_project
 
-router = APIRouter(prefix="/projects", tags=["projects"])
+router = APIRouter(prefix="/api/projects", tags=["projects"])
 
 @router.post("/analyze")
 async def analyze_snap_project(file: UploadFile):
