18313e447fe7515664a64d51ba39129a7c5c127b
manueljsanchezg <manueljsanchez10@gmail.com>
2026-02-22 21:35:00 +0100

    feat: Added session dependency and async attributes

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,15 +1,21 @@
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from typing import Annotated
+
+from sqlalchemy.ext.asyncio import AsyncSession, AsyncAttrs, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
+
+from fastapi import Depends
 
 engine = create_async_engine("sqlite+aiosqlite:///./test.db", echo=True)
 
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
-class Base(DeclarativeBase):
+class Base(DeclarativeBase, AsyncAttrs):
     pass
 
 
 async def get_session():
     async with async_session() as session:
         yield session
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
