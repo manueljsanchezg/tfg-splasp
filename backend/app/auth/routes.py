a099d593d2c618e0d8a72f1d4160bba279a63831
manueljsanchezg <manueljsanchez10@gmail.com>
2026-02-25 17:29:40 +0100

    feat: added register user

--- backend/app/auth/routes.py
+++ backend/app/auth/routes.py
@@ -8,13 +8,20 @@ from app.auth.schemas import LoginReq, Token
 
 router = APIRouter(prefix="/api/auth", tags=["auth"])
 
+@router.post("/register", response_model=Token)
+async def login(login_req: LoginReq, auth_service: AuthServiceDep):
+    token = await auth_service.register_user(login_req.username, login_req.password)
+    if token is None:
+        raise HTTPException(status_code=400, detail="Username already exists")
+    
+    return Token(access_token=token)
 
 @router.post("/login", response_model=Token)
 async def login(login_req: LoginReq, auth_service: AuthServiceDep):
-
     token = await auth_service.login_user(login_req.username, login_req.password)
     if token is None:
         raise HTTPException(status_code=400, detail="Invalid credentials")
+        
     return Token(access_token=token)
 
 
@@ -23,4 +30,5 @@ async def get_acess_token(form_data: Annotated[OAuth2PasswordRequestForm, Depend
     token = await auth_service.login_user(form_data.username, form_data.password)
     if token is None:
         raise HTTPException(status_code=400, detail="Invalid credentials")
+    
     return Token(access_token=token)
--- backend/app/auth/service.py
+++ backend/app/auth/service.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timezone, timedelta
 
-from app.utils import verify_password, generate_jwt
+from app.user.models import User
+from app.utils import verify_password, hash_password, generate_jwt
 from app.user.service import UserService
 
 
@@ -16,11 +17,33 @@ class AuthService:
         if not verify_password(password, existing_user.password):
             return None
         
-        expires_delta = timedelta(minutes=30)
+        expires_delta = timedelta(minutes=60)
         payload = {
             "sub": existing_user.username,
             "exp": datetime.now(timezone.utc) + expires_delta,
         }
+        token = generate_jwt(payload)
+
+        return token
+    
+    async def register_user(self, username: str, password: str):
+        existing_user = await self.user_service.get_by_username(username)
+
+        if existing_user:
+            return None
+        
+        hashed_password = hash_password(password)
+
+        user = User(username=username, password=hashed_password)
+
+        new_user = await self.user_service.save(user)
+
+        expires_delta = timedelta(minutes=60)
+        payload = {
+            "sub": new_user.username,
+            "exp": datetime.now(timezone.utc) + expires_delta,
+        }
+
         token = generate_jwt(payload)
 
         return token
