db0352d8b27ace45d8c585e598d30120e0f055b1
manueljsanchezg <manueljsanchez10@gmail.com>
2026-02-27 22:36:48 +0100

    feat: added session service

--- frontend/src/service/session.service.ts
+++ frontend/src/service/session.service.ts
@@ -1,10 +1,48 @@
-import { api } from "./api"
-import type { SessionResponse } from "../types/session"
+import { api } from './api'
+import type {
+  CloseSessionResponse,
+  JoinSessionResponse,
+  SessionData,
+  SessionResponse,
+} from '../types/session'
 
 export const getAllSessions = async (): Promise<SessionResponse[]> => {
   try {
     const response = await api.get<SessionResponse[]>('/sessions')
+    console.log(response.data)
     return response.data
+  } catch (error) {
+    console.error('Error getting sessions:', error)
+    throw error
+  }
+}
+
+export const createSession = async (data: SessionData): Promise<SessionResponse> => {
+  try {
+    const response = await api.post<SessionResponse>('/sessions', data)
+    console.log(response.data)
+    return response.data
+  } catch (error) {
+    console.error('Error getting sessions:', error)
+    throw error
+  }
+}
+
+export const joinSession = async (code: string): Promise<JoinSessionResponse> => {
+  try {
+    const response = await api.post<JoinSessionResponse>('/sessions/join', { code })
+    console.log(response.data)
+    return response.data
+  } catch (error) {
+    console.error('Error getting sessions:', error)
+    throw error
+  }
+}
+
+export const closeSession = async (sessionId: number): Promise<void> => {
+  try {
+    const response = await api.patch<CloseSessionResponse>('/sessions/join', { sessionId })
+    console.log(response.data)
   } catch (error) {
     console.error('Error getting sessions:', error)
     throw error
--- frontend/src/types/session.d.ts
+++ frontend/src/types/session.d.ts
@@ -1,10 +1,22 @@
-
-
 export interface SessionResponse {
-    id: number
-    name: string
-    code: string
-    startDate: string
-    endDate: string
-    isActive: boolean
+  id: number
+  name: string
+  code: string
+  startDate: string
+  endDate: string
+  isActive: boolean
+}
+
+export interface SessionData {
+  name: string
+  startDate: string
+  endDate: string
+}
+
+export interface JoinSessionResponse {
+  sessionId: number
+}
+
+export interface CloseSessionResponse {
+  message: string
 }
